fac64696f0379f68defbccc0042b1ce423a59f9c
Jean-Francois Dockes <jf@dockes.org>
2019-11-09 15:29:48 +0100

    looksok

--- therm.py
+++ therm.py
@@ -1,18 +1,139 @@
 #!/usr/bin/python3
 
 import sys
+import logging
+import os
 
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
-import thermui
 
+import thermui
+import conftree
+
+logging.basicConfig(level=5,
+                    format='%(asctime)s - %(name)s:%(lineno)d: %(message)s')
+logger = logging.getLogger()
 
 class Therm_Main(QWidget):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         self.ui = thermui.Ui_Form()
         self.ui.setupUi(self)
+        envconfname = 'THERM_CONFIG'
+        confname = None
+        if envconfname in os.environ:
+            confname = os.environ[envconfname]
+        if not confname:
+            QMessageBox.critical(self, "therm",
+                                 "No %s in environment" % envconfname)
+            sys.exit(1)
 
+        conf = conftree.ConfSimple(confname)
+
+        self.datarepo = conf.get('datarepo')
+        self.scratchdir = conf.get('scratchdir')
+        if not self.datarepo or not self.scratchdir:
+            QMessageBox.critical(self, "therm",
+                                 "No 'datarepo' or 'scratchdir' in config")
+            sys.exit(1)
+
+        self.remotesettingfile = os.path.join(self.datarepo, 'consigne')
+        self.myscratchact = os.path.join(self.scratchdir, 'ui')
+        self.myscratchdis = os.path.join(self.scratchdir, 'ui-')
+        self.ctlscratch = os.path.join(self.scratchdir, 'ctl')
+        if not os.path.exists(self.ctlscratch):
+            QMessageBox.critical(self, "therm",
+                                 "%s not found: controller not active" %
+                                 self.ctlscratch)
+            sys.exit(1)
+        self._readCtlValues()
+        cf = None
+        self.localactive = False
+        if os.path.exists(self.myscratchact):
+            # Local control
+            self.localactive = True
+            cf = conftree.ConfSimple(self.myscratchact)
+        elif os.path.exists(self.myscratchdis):
+            cf = conftree.ConfSimple(self.myscratchdis)
+        if cf:
+            self.localsetting = float(cf.get("localsetting") or 20.5)
+        else:
+            self.localsetting = 20.5
+
+        if self.localactive:
+            self.ui.commandePB.setChecked(True)
+            self.on_commandePB_toggled(True)
+        else:
+            self.ui.commandePB.setChecked(False)
+            self.on_commandePB_toggled(False)
+        self.lastDialValue = 0
+            
+        #with open(self.myscratch, 'w') as f:
+        #    print("localconsigne = %.2f" % self.lcdCurrent, file=f)
+
+    def _setlocalsetting(self):
+        try:
+            os.rename(self.myscratchdis, self.myscratchact)
+        except:
+            pass
+        with open(self.myscratchact, 'w') as f:
+            print("localsetting = %.2f" % self.localsetting, file=f)
+
+    def _setremotesetting(self):
+        try:
+            os.rename(self.myscratchact, self.myscratchdis)
+        except:
+            pass
+        
+    def _readCtlValues(self):
+        cf = conftree.ConfSimple(self.ctlscratch)
+        self.measuredtemp = float(cf.get("measuredtemp"))
+        self.remotesetting = open(self.remotesettingfile, 'r').read().strip()
+        self.remotesetting = float(self.remotesetting)
+        
+    def _manageUiState(self):
+        cmdlocal = self.ui.commandePB.isChecked()
+        displaysetting = self.ui.displayPB.isChecked()
+        if cmdlocal and displaysetting:
+            self.ui.dial.setEnabled(True)
+        else:
+            self.ui.dial.setEnabled(False)
+        if displaysetting:
+            if cmdlocal:
+                self.ui.lcdNumber.display(self.localsetting)
+            else:
+                self.ui.lcdNumber.display(self.remotesetting)
+        else:
+            self.ui.lcdNumber.display(self.measuredtemp)
+
+    def on_commandePB_toggled(self, checked):
+        logger.debug("on_commandePB_toggled: checked %d", checked)
+        if checked:
+            self.ui.commandePB.setText("Commande\nlocale")
+            self._setlocalsetting()
+        else:
+            self.ui.commandePB.setText("Commande\ndistante")
+            self._setremotesetting()
+        self._manageUiState()
+
+    def on_displayPB_toggled(self, checked):
+        logger.debug("on_displayPB_toggled: checked %d", checked)
+        if checked:
+            self.ui.displayPB.setText("Consigne")
+        else:
+            self.ui.displayPB.setText("Mesure")
+        self._manageUiState()
+            
+    def on_dial_sliderMoved(self, value):
+        logger.debug("dial: value %d", value)
+        delta = value - self.lastDialValue
+        self.lastDialValue = value
+        if delta > 10 or delta < -10:
+            return
+        self.localsetting += float(delta)/50.0
+        self._setlocalsetting()
+        self._manageUiState()
+        
 def main(args):
     app = QApplication(args)
 
---
+++ thermui.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'therm.ui'
+#
+# Created by: PyQt5 UI code generator 5.7
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(480, 320)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.dial = QtWidgets.QDial(Form)
+        self.dial.setObjectName("dial")
+        self.horizontalLayout.addWidget(self.dial)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.lcdNumber = QtWidgets.QLCDNumber(Form)
+        self.lcdNumber.setDigitCount(4)
+        self.lcdNumber.setSegmentStyle(QtWidgets.QLCDNumber.Flat)
+        self.lcdNumber.setProperty("value", 20.5)
+        self.lcdNumber.setObjectName("lcdNumber")
+        self.horizontalLayout.addWidget(self.lcdNumber)
+        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem2)
+        self.horizontalLayout.setStretch(1, 1)
+        self.horizontalLayout.setStretch(3, 2)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem3)
+        self.commandePB = QtWidgets.QPushButton(Form)
+        self.commandePB.setCheckable(True)
+        self.commandePB.setChecked(False)
+        self.commandePB.setObjectName("commandePB")
+        self.horizontalLayout_2.addWidget(self.commandePB)
+        spacerItem4 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem4)
+        self.displayPB = QtWidgets.QPushButton(Form)
+        self.displayPB.setCheckable(True)
+        self.displayPB.setObjectName("displayPB")
+        self.horizontalLayout_2.addWidget(self.displayPB)
+        spacerItem5 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem5)
+        self.horizontalLayout_2.setStretch(0, 1)
+        self.horizontalLayout_2.setStretch(1, 2)
+        self.horizontalLayout_2.setStretch(2, 2)
+        self.horizontalLayout_2.setStretch(3, 2)
+        self.horizontalLayout_2.setStretch(4, 1)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.verticalLayout.setStretch(0, 2)
+        self.verticalLayout.setStretch(1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.commandePB.setText(_translate("Form", "Commande\n"
+"distante"))
+        self.displayPB.setText(_translate("Form", "Mesure"))
+
